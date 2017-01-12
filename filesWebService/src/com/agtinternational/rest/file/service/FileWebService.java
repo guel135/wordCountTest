34850421a738f59e5d87c9e3b370f280295f628b
guel <guel135@gmail.com>
2017-01-12 19:38:55 +0100

    Added default parameters in webservice

--- filesWebService/src/com/agtinternational/rest/file/service/FileWebService.java
+++ filesWebService/src/com/agtinternational/rest/file/service/FileWebService.java
@@ -1,47 +1,54 @@
 package com.agtinternational.rest.file.service;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.List;
 
-import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/")
 public class FileWebService  {
 
-	@POST
-	@Path("/files")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response files(InputStream incomingData) {
-		StringBuilder crunchifyBuilder = new StringBuilder();
-		try {
-			BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
-			String line = null;
-			while ((line = in.readLine()) != null) {
-				crunchifyBuilder.append(line);
-			}
-		} catch (Exception e) {
-			System.out.println("Error Parsing: - ");
-		}
-		System.out.println("Data Received: " + crunchifyBuilder.toString());
- 
-		// return HTTP response 200 in case of success
-		return Response.status(200).entity(crunchifyBuilder.toString()).build();
-	}
+//	@PUT
+//	@Path("/files")
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	public Response files(InputStream incomingData) {
+//		StringBuilder crunchifyBuilder = new StringBuilder();
+//		try {
+//			BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
+//			String line = null;
+//			while ((line = in.readLine()) != null) {
+//				crunchifyBuilder.append(line);
+//			}
+//		} catch (Exception e) {
+//			System.out.println("Error Parsing: - ");
+//		}
+//		System.out.println("Data Received: " + crunchifyBuilder.toString());
+// 
+//		// return HTTP response 200 in case of success
+//		return Response.status(200).entity(crunchifyBuilder.toString()).build();
+//	}
 	@GET
-	@Path("/verify")
+	@Path("/files")
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response verifyRESTService(InputStream incomingData) {
-		String result = "CrunchifyRESTService Successfully started..";
+	
+	public Response verifyRESTService(@Context UriInfo info,
+	
+		
+		@QueryParam("url") String url,
+		@DefaultValue("100")@QueryParam("longerthan") long wordlimit,
+		@DefaultValue("50") @QueryParam("morethan") long wordamount) {
+
+		String result = "Hello Service";
  
 		// return HTTP response 200 in case of success
-		return Response.status(200).entity(result).build();
+		return Response.status(200).entity(result+" "+url+" "+wordamount+" "+wordlimit).build();
 	}
 
 }
