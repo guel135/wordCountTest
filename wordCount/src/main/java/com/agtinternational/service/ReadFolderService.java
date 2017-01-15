ff1c2debb78d6a1ff959162e3da9f646359b14bb
guel <guel135@gmail.com>
2017-01-15 16:43:09 +0100

    Delete temporal project used by development purposes

--- wordCount/src/main/java/com/agtinternational/service/ReadFolderService.java
+++
@@ -1,9 +0,0 @@
-package com.agtinternational.service;
-
-import com.agtinternational.entities.Directory;
-
-public interface ReadFolderService {
-
-	 public Directory read(String folder, long wordAmount, long wordRepeat);
-
-}
